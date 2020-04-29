df244ff22570572ee36d27f5c3f4881aad0d1c5c
maxiiis <maxiiiiis@yandex.ru>
2020-04-30 00:54:33 +0500

    + interface ISAPSettings

---
+++ Collector/ISAPSettings.cs
@@ -0,0 +1,25 @@
+﻿using ERPConnect; 
+
+namespace Collector
+{
+    /// <summary>
+    /// Интерфейс настройки параметров сборщика для SAP
+    /// </summary>
+    internal interface ISAPSettings
+    {
+        /// <summary>
+        /// Свойство подключения к системе SAP
+        /// </summary>
+        public R3Connection Connection { get; set; }
+
+        /// <summary>
+        /// Название функции SAP
+        /// </summary>
+        public string FunctionName { get; set; }
+
+        /// <summary>
+        /// Создание подключения к SAP
+        /// </summary>
+        public void CreateConnection(string connectionString);
+    }
+}
