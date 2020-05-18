09b8bf159aaf6040b1e4b23d96306bb1b32671bd
maxiiis <maxiiiiis@yandex.ru>
2020-05-18 20:36:03 +0500

    - Remove EventID from LogData table

--- EFModels/LogsDB/EventLog.cs
+++ EFModels/LogsDB/EventLog.cs
@@ -14,6 +14,6 @@ namespace EFModels.LogsDB
         public DateTime TimeStamp { get; set; }
         public int Cost { get; set; }
 
-        public EventLogData eventLogData { get; set; }
+        public EventLogData EventLogData { get; set; }
     }
 }
--- EFModels/LogsDB/EventLogData.cs
+++ EFModels/LogsDB/EventLogData.cs
@@ -5,12 +5,11 @@ namespace EFModels.LogsDB
     [Owned]
     public class EventLogData
     {
-        public long EventId { get; set; }
         public string ResourseFIO { get; set; }
         public string ResourseDepartment { get; set; }
         public string ResourseFilial { get; set; }
         public string ActivityText { get; set; }
         public string ActivityText2 { get; set; }
-        public EventLog eventLog { get; set; }
+        public EventLog EventLog { get; set; }
     }
 }
