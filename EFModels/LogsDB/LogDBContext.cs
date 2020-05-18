0912e02b0eca0e43d42fb25377f055079bd7b791
maxiiis <maxiiiiis@yandex.ru>
2020-05-18 16:09:58 +0500

    + Add EFModels Project with EF for PostgreSQL

---
+++ EFModels/LogsDB/LogDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFModels.LogsDB
+{
+    public class LogDBContext : DbContext
+    {
+        //public LogDBContext(DbContextOptions<LogDBContext> ConnectionString) : base(ConnectionString)
+        //{
+
+        //}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=LogDB;Username=postgres;Password=password");
+        }
+    }
+}
---
+++ EFModels/MainDB/MainDBContext.cs
@@ -0,0 +1,19 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+
+namespace EFModels.MainDB
+{
+    public class MainDBContext : DbContext
+    {
+        //public MainDBContext(DbContextOptions<MainDBContext> ConnectionString) : base(ConnectionString)
+        //{
+            
+        //}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=MainDB;Username=postgres;Password=password");
+        }
+    }
+}
