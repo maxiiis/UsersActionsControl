327e3a160d631a655f464b35de6225990da18845
maxiiis <maxiiiiis@yandex.ru>
2020-05-28 19:43:01 +0500

    GraphX examples remove comments

--- UI/Models/DataVertex.cs
+++ UI/Models/DataVertex.cs
@@ -2,19 +2,8 @@
 
 namespace UI.Models
 {
-    /* DataVertex is the data class for the vertices. It contains all custom vertex data specified by the user.
-     * This class also must be derived from VertexBase that provides properties and methods mandatory for
-     * correct GraphX operations.
-     * Some of the useful VertexBase members are:
-     *  - ID property that stores unique positive identfication number. Property must be filled by user.
-     *  
-     */
-
     public class DataVertex: VertexBase
     {
-        /// <summary>
-        /// Some string property for example purposes
-        /// </summary>
         public string Text { get; set; }
  
         #region Calculated or static props
@@ -26,10 +15,6 @@ namespace UI.Models
 
         #endregion
 
-        /// <summary>
-        /// Default parameterless constructor for this class
-        /// (required for YAXLib serialization)
-        /// </summary>
         public DataVertex():this(string.Empty)
         {
         }
