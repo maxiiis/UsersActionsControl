f3bbe4365e43fbc039614eb70f097ce6745d857b
maxiiis <maxiiiiis@yandex.ru>
2020-05-28 14:25:14 +0500

    + CaseBuilder

---
+++ EFModels/CaseBuilder.cs
@@ -0,0 +1,55 @@
+﻿using EFModels.LogsDB;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EFModels
+{
+    public class CaseBuilder
+    {
+        public Case CreateCase(long CaseId)
+        {
+            LogDBContext logDB = new LogDBContext();
+
+            var Events = logDB.EventLogs.Where(b => b.CaseId == CaseId).OrderBy(b => b.TimeStamp).ToList();
+
+            Case newCase = new Case(CaseId);
+
+            foreach (var e in Events)
+            {
+                newCase.Add(new Event(e.Activity) { TimeStamp = e.TimeStamp });
+            }
+
+            return newCase;
+        }
+
+        public Case CreateGeneralCase()
+        {
+            LogDBContext logDB = new LogDBContext();
+            var s = logDB.EventLogs.OrderBy(s => s.CaseId).ThenBy(s => s.TimeStamp).ToList();
+
+            var groups = s.GroupBy(s => s.CaseId);
+
+            List<Case> cases = new List<Case>();
+
+            Case general = new Case(-1);
+
+            foreach (var g in groups)
+            {
+                Case bP = cases.FirstOrDefault(f => f.CaseId == g.Key);
+
+                if (bP == null)
+                {
+                    bP = new Case(g.Key);
+                    cases.Add(bP);
+                }
+
+                foreach (var e in g)
+                {
+                    bP.Add(new Event(e.Activity) { TimeStamp = e.TimeStamp });
+                    general.UpdateHierarchy(bP.Events.Last());
+                }
+            }
+            return general;
+        }
+    }
+}
